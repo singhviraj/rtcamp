504cc21900f2551ba2a09f6a478a75597f290f23
Viraj <testmail2698@gmail.com>
2023-01-26 02:28:29 -0800

    added password feature in index.php

--- index.php
+++ index.php
@@ -8,20 +8,25 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
         <meta charset="UTF-8">
         <title></title>
         <?php
-        $name = $email = $gender = $comment = $website = "";
+        $name = $email = $gender = $password1 =$password2 = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $name = test_input($_POST["name"]);
   $email = test_input($_POST["email"]);
-  $gender = test_input($_POST["gender"]);
-  if(filter_var($email ,FILTER_VALIDATE_EMAIL)== TRUE && empty($name) == FALSE && empty($gender) == FALSE){
+  $password1 = test_input($_POST["password1"]);
+  $password2 = test_input($_POST["password2"]);
+  if(filter_var($email ,FILTER_VALIDATE_EMAIL)== TRUE && empty($name) == FALSE && $password1 == $password2){
+      
       $correctemail ="<html>
     <head>
        <title></title> </head>
     <body>
-    <form method ='post' action ='newEmptyPHP.php'>
-    <h4>Perfect! the details are valid . To verify yourself with the 2 step verification kindly click on the button below</h4>
-    <input type='submit' name='submitcode' value ='Submit'>  
+    <form method ='post' action ='verify2sv.php'>
+    To verify if <input type='text' name='name' value =$name> is the owner of email"
+              . " <input type='text' name='email' value =$email> we have send a one time code ,
+                  to receive the code kindly click on the button below
+    <input type='submit' name='submitcode' value ='Submit'> 
+    
     </form>
     </body>
 </html>";
@@ -31,6 +36,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   if(filter_var($email ,FILTER_VALIDATE_EMAIL)== FALSE){
       $incorrectemail ="email is incorrect . Kindly try again";
       echo $incorrectemail;
+  }
+  if($password1!=$password2){
+      echo"kindly check the passwords";
   }
 }
 
@@ -49,18 +57,17 @@ function test_input($data) {
   <br><br>
   E-mail: <input type="text" name="email">
   <br><br>
-  Gender:
-  <input type="radio" name="gender" value="female">Female
-  <input type="radio" name="gender" value="male">Male
-  <input type="radio" name="gender" value="other">Other
+  Password: <input type="password" name="password1">
   <br><br>
-  <input type="submit" name="submit" value="Submit"> 
+  Enter password again: <input type="password" name="password2">
+  <br><br>
+   <input type="submit" name="submit" value="Submit"> 
+</form>
     </body>
 </html>
   <?php
 echo "<h2>Enter your 2sv code here:</h2>";
 echo "your name".$name;
 echo "your name".$email;
-echo "your name".$gender;
 
      ?>
