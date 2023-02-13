633ee574bba1c90b28216a57e1fb71d5fecfe7a6
Viraj <testmail2698@gmail.com>
2023-02-13 03:49:50 -0800

    different message in stopcode

--- stopcode.php
+++ stopcode.php
@@ -6,7 +6,7 @@
  */
 if (isset($_SESSION['code1'])){
          $_SESSION["code1"] = 0;
-     echo"emails stopped for this user";    
+     echo"kindly login again to start the emails";    
      }
      else{
          echo"Currently,there were no emails running for this user .To stop the emails for this user you need to first start the emails";
